8cc65846aa01d5196737cad5f26649511f2bf563
David Timms <davidtimms@btinternet.com>
2023-12-03 22:29:15 +0000

    Complete day 1 part 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -2,9 +2,15 @@ module Day1
     ( part1
     ) where
 
+import Data.Maybe
+import Data.Char
+import Data.List
+
 part1 :: [String] -> String
 part1 puzzleLines = show $ sum $ map extractCalibrationValue puzzleLines
 
 
 extractCalibrationValue :: String -> Integer
-extractCalibrationValue _ = 1
+extractCalibrationValue puzzleLine = read $ firstDigit ++ lastDigit where
+    firstDigit = maybeToList $ find isDigit puzzleLine
+    lastDigit = maybeToList $ find isDigit $ reverse puzzleLine
